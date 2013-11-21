5e6b2408338df224f0626442f6be3c823f5daf4a
Sohaib Muhammad <sohaib770@gmail.com>
2013-11-22 00:52:34 +0500

    Initial Commit

---
+++ MovieQuotes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieQuotes
+//
+//  Created by Sohaib Muhammad on 22/11/2013.
+//  Copyright (c) 2013 iDevNerds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieQuotesTests/MovieQuotesTests.m
@@ -0,0 +1,34 @@
+//
+//  MovieQuotesTests.m
+//  MovieQuotesTests
+//
+//  Created by Sohaib Muhammad on 22/11/2013.
+//  Copyright (c) 2013 iDevNerds. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MovieQuotesTests : XCTestCase
+
+@end
+
+@implementation MovieQuotesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
